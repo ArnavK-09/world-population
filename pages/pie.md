433f5e6fe369766d4b391d785da3d5454b66bc14
Arnav K <arnavkaushal09@gmail.com>
2024-03-18 15:04:23 +0000

    Dropdown in pie.md

--- pages/pie.md
+++ pages/pie.md
@@ -3,22 +3,17 @@
 <|container|
 
 # 🥧 World **Population**{: .color-primary} Pie Exploration!
+<br />
+<|1 1|layout|
+
+### Choose **Year**{: .color-primary}!
+
+<|{year}|selector|lov={YEARS}|dropdown|on_change=update_year|>
+
+|>
 
 <br />
-### **2022**{: .color-primary} Population
-<|{pie_of_2022}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **2020**{: .color-primary} Population
-<|{pie_of_2020}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **2015**{: .color-primary} Population
-<|{pie_of_2015}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **2010**{: .color-primary} Population
-<|{pie_of_2010}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **2000**{: .color-primary} Population
-<|{pie_of_2000}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **1990**{: .color-primary} Population
-<|{pie_of_1990}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **1980**{: .color-primary} Population
-<|{pie_of_1980}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
-### **1970**{: .color-primary} Population
-<|{pie_of_1970}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
+### **<|{year}|text|raw=True|>**{: .color-primary} Population!
+
+<|{pie_data}|chart|type=pie|values=Population|options={pie_options}|labels=Country|>
 |>
--- pages/pie.py
+++ pages/pie.py
@@ -5,52 +5,28 @@ import pandas as pd
 # Our data file 
 DATA = pd.read_csv("./data.csv")
 
+# Year 
+years_list = list(filter(lambda x: x.endswith("Population"), DATA.columns)) # All years present in dataset columns
+YEARS = list(map(lambda x: x.replace("Population", "").strip(), years_list))
+year = YEARS[0]
+
 # Excrating Pie Charts Data
-pie_of_2022 = {
+pie_data = {
   "Country": list(DATA["Country/Territory"]),
   "Population": list(DATA["2022 Population"])
 }
 
-pie_of_2020 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["2020 Population"])
-}
-
-pie_of_2015 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["2015 Population"])
-}
-
-pie_of_2010 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["2010 Population"])
-}
-
-pie_of_2000 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["2000 Population"])
-}
-
-pie_of_1990 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["1990 Population"])
-}
-
-pie_of_1980 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["1980 Population"])
-}
-
-pie_of_1970 = {
-  "Country": list(DATA["Country/Territory"]),
-  "Population": list(DATA["1970 Population"])
-}
+# Selection Handler 
+def update_year(state):
+    state.pie_data = {
+      "Country": list(DATA["Country/Territory"]),
+      "Population": list(DATA[f"{state.year} Population"])
+    }
 
 # Pie configuration
 pie_options = {
     "textinfo": "none"
 }
-
 
 # Creating Page 
 pie_page = Markdown("pie.md")
